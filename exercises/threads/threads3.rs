a7e1cc9f00a072012be61706044bd562f04a9854
shadrach-tayo <shadrachtemitayo@gmail.com>
2023-02-03 03:09:44 +0100

    completed thread exercises

--- exercises/threads/threads3.rs
+++ exercises/threads/threads3.rs
@@ -1,8 +1,7 @@
 // threads3.rs
 // Execute `rustlings hint threads3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
+use std::sync::Mutex;
 use std::sync::mpsc;
 use std::sync::Arc;
 use std::thread;
@@ -28,22 +27,28 @@ fn send_tx(q: Queue, tx: mpsc::Sender<u32>) -> () {
     let qc = Arc::new(q);
     let qc1 = Arc::clone(&qc);
     let qc2 = Arc::clone(&qc);
-
-    thread::spawn(move || {
+    let shared_tx = tx.clone();
+    // let shared_tx = shared_tx.clone();
+    
+    
+    let handle = thread::spawn(move || {
         for val in &qc1.first_half {
             println!("sending {:?}", val);
-            tx.send(*val).unwrap();
+            shared_tx.send(*val).unwrap();
             thread::sleep(Duration::from_secs(1));
         }
     });
+    handle.join().unwrap();
 
-    thread::spawn(move || {
+    let shared_tx2 = tx.clone();
+    let handle2 = thread::spawn(move || {
         for val in &qc2.second_half {
             println!("sending {:?}", val);
-            tx.send(*val).unwrap();
+            shared_tx2.send(*val).unwrap();
             thread::sleep(Duration::from_secs(1));
         }
     });
+    handle2.join().unwrap();
 }
 
 fn main() {
